0b3876f5527ce821f98b050b388dfd56714f36e1
Helmi Griffiths <t3grhe00@students.oamk.fi>
2025-03-20 10:49:59 +0200

    Connected firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,9 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    // alias(libs.plugins.hilt) // Apply Hilt plugin from version catalog
+    id("com.google.gms.google-services")
+    // id("org.jetbrains.kotlin.kapt") // Added to enable kapt for Hilt
 }
 
 android {
@@ -41,6 +44,9 @@ android {
 
 dependencies {
 
+    // Add the dependencies for any other desired Firebase products
+    // https://firebase.google.com/docs/android/setup#available-libraries
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -49,6 +55,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.coil.compose) // Added Coil
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -56,4 +63,18 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // Import the Firebase BoM
+    implementation(platform(libs.firebase.bom))
+    // When using the BoM, don't specify versions in Firebase dependencies
+    implementation(libs.firebase.analytics)
+
+    implementation(libs.firebase.auth.ktx)
+    implementation(libs.firebase.firestore.ktx)
+    implementation(libs.hilt.android)
+    // kapt(libs.hilt.compiler)
 }
+
+/* kapt {
+    correctErrorTypes = true // Ensures better error messages from Hilt
+} */
